6ba561faecc3cabbb332fdfb71d81b8de469fe3c
javad pournosrat <pournosrat.javad@gmail.com>
2021-04-13 01:00:34 +0430

    add range port in input!

--- portscanner.py
+++ portscanner.py
@@ -4,12 +4,28 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.setdefaulttimeout(2)
-host = input(str('[*]enter host to scan:'))
-port = int(input('[*]enter port number to scan:'))
 
-def portscanner(port):
-    if sock.connect_ex((host, port)):
-        print(f'port {port} is clodes')
-    else:
-        print(f'port {port} is open')
-portscanner(port)
+host = input(str('[*]enter host to scan:'))
+
+sporrport = input(str('''single port or range port:
+    1 = single port
+    2 = range port
+    :'''))
+if sporrport == '1':
+    port = int(input('[*]enter port number to scan:'))
+    def portscanner(port):
+        if sock.connect_ex((host, port)):
+            print(f'port {port} is clodes')
+        else:
+            print(f'port {port} is open')
+    portscanner(port)
+elif sporrport == '2':
+    start = int(input('enter start of range:'))
+    end = int(input('enter end of range:'))
+    for port in range(start, end):
+        def portscanner(port):
+            if sock.connect_ex((host, port)):
+                print(f'port {port} is clodes')
+            else:
+                print(f'port {port} is open')
+        portscanner(port)
