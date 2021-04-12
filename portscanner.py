e1cf05c00ddddc5e1e19634b36492681dc6c96f2
javad pournosrat <pournosrat.javad@gmail.com>
2021-04-13 00:42:53 +0430

    add set timeout

--- portscanner.py
+++ portscanner.py
@@ -3,9 +3,9 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-host = input(str('enter host:'))
-port = int(input('enter port number:'))
+socket.setdefaulttimeout(2)
+host = input(str('[*]enter host to scan:'))
+port = int(input('[*]enter port number to scan:'))
 
 def portscanner(port):
     if sock.connect_ex((host, port)):
