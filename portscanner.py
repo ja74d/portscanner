161e54492e039353259292dc0f4d52e3d6e6ddb4
javad pournosrat <pournosrat.javad@gmail.com>
2021-04-13 01:04:00 +0430

    add def for portscanner!

--- portscanner.py
+++ portscanner.py
@@ -11,21 +11,17 @@ sporrport = input(str('''single port or range port:
     1 = single port
     2 = range port
     :'''))
+#def part!
+def portscanner(port):
+    if sock.connect_ex((host, port)):
+        print(f'port {port} is clodes')
+    else:
+        print(f'port {port} is open')
 if sporrport == '1':
     port = int(input('[*]enter port number to scan:'))
-    def portscanner(port):
-        if sock.connect_ex((host, port)):
-            print(f'port {port} is clodes')
-        else:
-            print(f'port {port} is open')
     portscanner(port)
 elif sporrport == '2':
     start = int(input('enter start of range:'))
     end = int(input('enter end of range:'))
     for port in range(start, end):
-        def portscanner(port):
-            if sock.connect_ex((host, port)):
-                print(f'port {port} is clodes')
-            else:
-                print(f'port {port} is open')
         portscanner(port)
